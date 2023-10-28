78b5766a1febc93922d9e3f0b5dffecf8cc8d8c0
Nasabae <ssakoto11@gmail.com>
2023-10-27 17:00:20 -0700

    1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,11 @@
-#!/usr/bin/pup
-# Install this specific version of flask (2.1.0)
+# Install Werkzeug 2.1.1 
+package { 'Werkzeug':
+ensure   => '2.1.1',
+provider => 'pip3',
+}
+
+# Install flask from pip3
 package {'flask':
   ensure   => '2.1.0',
-  provider => 'pip3'
+  provider => 'pip3',
 }
